4caf8289f8966574a3be07c77a82c4e778a3bedb
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-05-27 22:31:02 +0530

    update logic

--- DAA-4th_SEM/4_floydwarshell.c
+++ DAA-4th_SEM/4_floydwarshell.c
@@ -1,50 +1,44 @@
-// Floyd-Warshall Algorithm in C
+#include<stdio.h>
 
-#include <stdio.h>
 
-// defining the number of vertices
-#define nV 4
-
-#define INF 999
-
-void printMatrix(int matrix[][nV]);
-
-// Implementing floyd warshall algorithm
-void floydWarshall(int graph[][nV]) {
-  int matrix[nV][nV], i, j, k;
-
-  for (i = 0; i < nV; i++)
-    for (j = 0; j < nV; j++)
-      matrix[i][j] = graph[i][j];
-
-  // Adding vertices individually
-  for (k = 0; k < nV; k++) {
-    for (i = 0; i < nV; i++) {
-      for (j = 0; j < nV; j++) {
-        if (matrix[i][k] + matrix[k][j] < matrix[i][j])
-          matrix[i][j] = matrix[i][k] + matrix[k][j];
-      }
-    }
-  }
-  printMatrix(matrix);
+int min(int a, int b){
+    return (a<b)?a:b;
 }
 
-void printMatrix(int matrix[][nV]) {
-  for (int i = 0; i < nV; i++) {
-    for (int j = 0; j < nV; j++) {
-      if (matrix[i][j] == INF)
-        printf("%4s", "INF");
-      else
-        printf("%4d", matrix[i][j]);
+void floyd_warshall(int graph[100][100], int n){
+    int dist[100][100];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            dist[i][j]=graph[i][j];
+        }
     }
-    printf("\n");
-  }
+    for(int k=0; k<n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+        }
+    }
+
+    printf("THE SHORTEST PATHS BETWEEN EVERY PAIR OF VERTICES ARE: \n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%2d ",dist[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-int main() {
-  int graph[nV][nV] = {{0, INF,3,INF},
-                    {2, 0, INF, INF},
-                    {INF, 7, 0, 1},
-                    {6, INF,INF, 0}};
-  floydWarshall(graph);
+int main(){
+    int n;
+    printf("ENTER THE NUMBER OF VERTICES: ");
+    scanf("%d",&n);
+    int graph[100][100];
+    printf("ENTER THE ADJACENCY MATRIX: \n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d",&graph[i][j]);
+        }
+    }
+    floyd_warshall(graph,n);
 }
