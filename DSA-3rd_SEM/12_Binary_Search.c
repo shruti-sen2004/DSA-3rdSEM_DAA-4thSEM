86170958dfc3a30dec501ebacdf9bcce7fe6560e
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-05-27 16:35:54 +0530

    Binary Search - Iterative and recurssive way

--- DSA-3rd_SEM/12_Binary_Search.c
+++ DSA-3rd_SEM/12_Binary_Search.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int binary_search(int arr[],int n, int key){
+int binary_search_itr(int arr[],int n, int key){        //ITERATIVE BINARY SEARCH
     int low=0;
     int high = n-1;
     while(low<=high){
@@ -18,6 +18,22 @@ int binary_search(int arr[],int n, int key){
     return -1;
 }
 
+int binary_search_rec(int arr[],int low, int high, int key){        //RECURSIVE BINARY SEARCH
+    if(low>high){
+        return -1;
+    }
+    int mid = (low+high)/2;
+    if(arr[mid]==key){
+        return mid+1;
+    }
+    else if(arr[mid]<key){
+        return binary_search_rec(arr,mid+1,high,key);
+    }
+    else{
+        return binary_search_rec(arr,low,mid-1,key);
+    }
+}
+
 int main(){
     int n;
     printf("ENTER THE SIZE OF THE ARRAY: ");
@@ -32,6 +48,8 @@ int main(){
     printf("ENTER THE ELEMENT TO BE SEARCHED: ");
     scanf("%d",&key);
 
-    int result = binary_search(arr,n,key);
-    (result==-1)?printf("ELEMENT NOT FOUND"):printf("ELEMENT FOUND AT INDEX %d",result);
+    int result_itr = binary_search_itr(arr,n,key);
+    int result_rec = binary_search_rec(arr,0,n-1,key);
+    (result_itr==-1)?printf("\nELEMENT NOT FOUND ITR"):printf("\nELEMENT FOUND ITERATIVELY AT INDEX %d",result_itr);
+    (result_rec==-1)?printf("\nELEMENT NOT FOUND REC"):printf("\nELEMENT FOUND RECCURSIVELY AT INDEX %d",result_rec);
 }
