550fc32a92ad7520b31303c6749cd2a27af59e55
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-05-29 10:47:31 +0530

    Update code

--- DAA-4th_SEM/6_Bellman_ford.c
+++ DAA-4th_SEM/6_Bellman_ford.c
@@ -1,54 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
+#define INFINITY 9999
+#define MAX 10
 
-#define MAX_VERTICES 100
-#define MAX_EDGES 100
+void bellmanford(int G[MAX][MAX], int n, int startnode);
 
-
-void bellmanFord(int graph[MAX_EDGES][3], int V, int E, int source) {
-	int i, j;
-    int distance[MAX_VERTICES];
-    for (i = 0; i < V; i++)
-        distance[i] = INT_MAX;
-    distance[source] = 0; 
-
-    
-    for (i = 1; i <= V - 1; i++) {
-        for (j = 0; j < E; j++) {
-            int u = graph[j][0];
-            int v = graph[j][1];
-            int weight = graph[j][2];
-            if (distance[u] != INT_MAX && distance[u] + weight < distance[v])
-                distance[v] = distance[u] + weight;
+int main(){
+    int G[MAX][MAX], i, j, n, u;
+    printf("ENTER THE NUMBER OF VERTICES: ");
+    scanf("%d",&n);
+    printf("\nENTER THE ADJACENCY MATRIX:\n");
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            scanf("%d",&G[i][j]);
         }
     }
-
-   
-    for (i = 0; i < E; i++) {
-        int u = graph[i][0];
-        int v = graph[i][1];
-        int weight = graph[i][2];
-        if (distance[u] != INT_MAX && distance[u] + weight < distance[v]) {
-            printf("Graph contains negative weight cycle\n");
-            return;
-        }
-    }
-
-   
-    printf("Vertex   Distance from Source\n");
-    for (i = 0; i < V; i++)
-        printf("%d \t\t %d\n", i, distance[i]);
+    printf("\nENTER THE STARTING NODE: ");
+    scanf("%d",&u);
+    bellmanford(G,n,u);
+    return 0;
 }
 
-int main() {
-    int V = 6;
-    int E = 9; 
-    int graph[MAX_EDGES][3] = {{0, 1, 6}, {0, 2, 4}, {0, 3, 5}, {1, 4, -1}, {2, 1, -2}, {2, 4, 3}, {3, 2, -2}, {3, 5, -1}, {4, 5, 3}};
-    int source = 0; 
-
-    bellmanFord(graph, V, E, source);
-
-    return 0;
+void bellmanford(int G[MAX][MAX], int n, int startnode){
+    int distance[MAX], pred[MAX];
+    for(int i = 0; i < n; i++){              //INITIALIZE DISTANCE AND PRED ARRAY
+        distance[i] = INFINITY;
+        pred[i] = startnode;
+    }
+    distance[startnode] = 0;
+    for(int i = 0; i < n-1; i++){            //RELAX ALL EDGES N-1 TIMES
+        for(int u = 0; u < n; u++){
+            for(int v = 0; v < n; v++){
+                if(G[u][v] != 0){
+                    if(distance[u] + G[u][v] < distance[v]){
+                        distance[v] = distance[u] + G[u][v];
+                        pred[v] = u;
+                    }
+                }
+            }
+        }
+    }
+    for(int u = 0; u < n; u++){              //CHECK FOR NEGATIVE CYCLE
+        for(int v = 0; v < n; v++){
+            if(G[u][v] != 0){
+                if(distance[u] + G[u][v] < distance[v]){
+                    printf("NEGATIVE CYCLE DETECTED\n");
+                    return;
+                }
+            }
+        }
+    }
+    printf("DISTANCE FROM NODE %d TO OTHER NODES:\n",startnode);
+    for(int i = 0; i < n; i++){
+        printf("NODE %d: %d\n",i,distance[i]);
+    }
 }
